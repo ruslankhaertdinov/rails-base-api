b3159e98bed01639852077cb4104cab1f49d76b4
Artem Biserov <artem.biserov@flatstack.com>
2014-08-14 13:53:18 +0400

    comment example controller specs

--- spec/controllers/example_controller_spec.rb
+++ spec/controllers/example_controller_spec.rb
@@ -1,67 +1,67 @@
-require 'rails_helper'
-
-class ExampleController < ApplicationController
-end
-
-describe ExampleController do
-  context 'when raise bad request error' do
-    controller do
-      def index
-        raise ActionController::BadRequest
-      end
-    end
-
-    it 'renders bad request response' do
-      #expect(controller).to receive(:render_bad_request_error)
-      get :index, format: :json
-      binding.pry
-    end
-  end
-
-  context 'when raise bad request error' do
-    controller do
-      def index
-        raise ActiveRecord::RecordNotFound
-      end
-    end
-
-    it 'renders bad request response' do
-      #expect(controller).to receive(:render_not_found_error)
-      get :index, format: :json
-      binding.pry
-    end
-  end
-
-  context 'when raise bad request error' do
-    controller do
-      def index
-        user = User.new
-        user.save
-        raise ActiveRecord::RecordInvalid.new(user)
-      end
-    end
-
-    it 'renders bad request response' do
-      #expect(controller).to receive(:render_validation_error)
-      #get :index, format: :json
-      #binding.pry
-    end
-  end
-
-  context 'validation error' do
-    controller do
-      def index
-        user = FactoryGirl.create :user
-        user.email = ''
-        user.save
-        respond_with user
-      end
-    end
-
-    it 'renders bad request response' do
-      #expect(controller).to receive(:render_validation_error)
-      post :index, format: :json
-      binding.pry
-    end
-  end
-end
+#require 'rails_helper'
+#
+#class ExampleController < ApplicationController
+#end
+#
+#describe ExampleController do
+#  context 'when raise bad request error' do
+#    controller do
+#      def index
+#        raise ActionController::BadRequest
+#      end
+#    end
+#
+#    it 'renders bad request response' do
+#      #expect(controller).to receive(:render_bad_request_error)
+#      get :index, format: :json
+#      binding.pry
+#    end
+#  end
+#
+#  context 'when raise bad request error' do
+#    controller do
+#      def index
+#        raise ActiveRecord::RecordNotFound
+#      end
+#    end
+#
+#    it 'renders bad request response' do
+#      #expect(controller).to receive(:render_not_found_error)
+#      get :index, format: :json
+#      binding.pry
+#    end
+#  end
+#
+#  context 'when raise bad request error' do
+#    controller do
+#      def index
+#        user = User.new
+#        user.save
+#        raise ActiveRecord::RecordInvalid.new(user)
+#      end
+#    end
+#
+#    it 'renders bad request response' do
+#      #expect(controller).to receive(:render_validation_error)
+#      #get :index, format: :json
+#      #binding.pry
+#    end
+#  end
+#
+#  context 'validation error' do
+#    controller do
+#      def index
+#        user = FactoryGirl.create :user
+#        user.email = ''
+#        user.save
+#        respond_with user
+#      end
+#    end
+#
+#    it 'renders bad request response' do
+#      #expect(controller).to receive(:render_validation_error)
+#      post :index, format: :json
+#      binding.pry
+#    end
+#  end
+#end
