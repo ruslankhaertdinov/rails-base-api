b9f4a27ff1cc3b521328c2fd52176fcd4e1b377e
Airat Shigapov <airat.shigapov@flatstack.com>
2014-12-02 19:35:36 +0300

    Upgrade interactor-rails gem and fix broken compatibility

--- app/interactors/authenticate_user.rb
+++ app/interactors/authenticate_user.rb
@@ -3,14 +3,14 @@ class AuthenticateUser
 
   OPTIONS = { store: false, scope: :user }
 
-  def perform
-    context[:user] = authenticated_user
+  def call
+    context.user = authenticated_user
   end
 
   private
 
   def authenticated_user
-    warden.authenticate!(OPTIONS).tap do |user|
+    context.warden.authenticate!(OPTIONS).tap do |user|
       user.ensure_authentication_token!
     end
   end
